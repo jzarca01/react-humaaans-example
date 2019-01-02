b81d2cf8a0d35a1b5b937502937b1a5105c4d6a0
Jérémie Zarca <jeremie.zarca@gmail.com>
2019-01-02 16:14:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import tinycolor from 'tinycolor2';
+
+import { elements } from './elements';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 1,
+      colors: this.createRandomColors()
+    };
+    this.changeHuman = this.changeHuman.bind(this);
+    this.createRandomColors = this.createRandomColors.bind(this);
+    this.renderRandomHuman = this.renderRandomHuman.bind(this);
+  }
+
+  changeHuman() {
+    this.setState({
+      count: this.state.count + 1,
+      colors: this.createRandomColors()
+    });
+  }
+
+  renderRandomHuman() {
+    const positionArray = [
+      {
+        name: 'Standing',
+        modulo: 24
+      },
+      {
+        name: 'Sitting',
+        modulo: 8
+      }
+    ];
+    const { count, colors } = this.state;
+    return React.createElement(
+      elements[
+        `${positionArray[count % 2].name}${(count %
+          positionArray[count % 2].modulo) +
+          1}`
+      ],
+      {
+        hairColor: colors[0],
+        skinColor: colors[1],
+        shoeColor: colors[2],
+        pantColor: colors[3],
+        shirtColor: colors[4],
+        coatColor: colors[5],
+        objectColor: colors[6],
+        wheelchairColor: colors[7],
+        hatColor: colors[8]
+      }
+    );
+  }
+
+  createRandomColors() {
+    const randomColor = tinycolor.random();
+    const colors = randomColor.monochromatic(10);
+    return colors.map(t => t.toHexString());
+  }
+
+  render() {
+    return (
+      <div className="app" style={styles.container}>
+        <div style={styles.welcome}>React-Humaaans Demo</div>
+        <div style={styles.welcome} onClick={this.changeHuman}>
+          {this.renderRandomHuman()}
+        </div>
+      </div>
+    );
+  }
+}
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+};
+
+export default App;
